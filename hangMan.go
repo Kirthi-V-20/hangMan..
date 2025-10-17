e6111f476493e339367ceb0c2346f218f9d7beb0
KIRUTHIKA <kirthi2001va@gmail.com>
2025-10-17 13:45:52 +0530

    Print all words line by line

--- hangMan.go
+++ hangMan.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func getSecretWord(wordFileName string) string {
+	file, err := os.Open(wordFileName)
+	if err != nil {
+		panic(fmt.Sprintf("The file could not be opened: %v", err))
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
 	return "Hii"
 }
 func main() {
