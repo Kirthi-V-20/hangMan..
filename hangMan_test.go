5d89b6f48d0ab9d8ca9a85ee06d28339ab63f21f
KIRUTHIKA <kirthi2001va@gmail.com>
2025-10-17 13:52:23 +0530

    RED: Adds test case for check secret word has atleast >=6 letters

--- hangMan_test.go
+++ hangMan_test.go
@@ -12,3 +12,11 @@ func TestSecretWordNoCapitals(t *testing.T) {
 		t.Errorf("Should not get words with capital letters. Got %s", secretWord)
 	}
 }
+
+func TestSecretWordLength(t *testing.T) {
+	wordList := "/usr/share/dict/words"
+	secretWord := getSecretWord(wordList)
+	if len(secretWord) < 6 {
+		t.Errorf("Expected word length 6 or greater than 6, but got %q (length %d)", secretWord, len(secretWord))
+	}
+}
